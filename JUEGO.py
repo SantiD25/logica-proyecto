8673cb52c0448384da8d49dd54ea44f62944774c
santiD25 <sabtiduque2013@gmail.com>
2023-06-08 10:19:35 -0500

    juego

--- JUEGO.py
+++ JUEGO.py
@@ -1,4 +1,26 @@
 print("este es el archivo para el juego")
+import random
 
-
-      
+class Adivina_adivinador:
+    def __init__(self):
+        self.palabras = ["manzana", "pera", "mango","piña", "poma", "ceresa", "coco", "kiwi", "limon", "banano", "maracuya", "mora", "sandia", "uva", "ciruela", "fresa", "papaya", "mandarina", "naranja", "lulo", "guanabana", "mamoncillo", "guayaba", "maracuya", "melocoton"]
+        self.palabra = random.choice(self.palabras)
+        self.letras_palabras = set(self.palabra)
+        self.letras_encontradas = set()
+        self.vidas = 6
+        
+        #self.jugar()
+    
+    def jugar(self):
+        print("BIENVENIDO A ADIVINA_ADIVINADOR!")
+        print("adivina la futa")
+        
+        while self.vidas > 0 and self.letras_palabras:
+            self.mostrar_palabra()
+            letra = self.conseguir_letra()
+            self.probar_letra(letra)
+        
+        if self.letras_palabras:
+            print("Perdiste la palabra era:", self.palabra)
+        else:
+            print("¡Felicidades adivinaste! la palabra:", self.palabra)
